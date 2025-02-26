6f8b9ba6d1a7dbbf8d017265d4c2b584fe01479d
rajKetan <147344386+rajKetan@users.noreply.github.com>
2025-02-27 00:30:41 +0530

    change in spark file

--- Spark.java
+++ Spark.java
@@ -2,7 +2,7 @@ public class Spark {
     public static void main(String[] args) {
         System.out.println("Hello");
     }
-    public void m1(){
+    public void payment(){
         System.out.println("m1");
     }
 
