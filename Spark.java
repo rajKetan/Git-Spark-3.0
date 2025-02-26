4fee476dce6270b40f29cba0d4249b3966220c41
rajKetan <147344386+rajKetan@users.noreply.github.com>
2025-02-26 23:33:12 +0530

    change in spark

--- Spark.java
+++ Spark.java
@@ -2,6 +2,9 @@ public class Spark {
     public static void main(String[] args) {
         System.out.println("Hello");
     }
+    public void m1(){
+        System.out.println("m1");
+    }
 
     
 }
